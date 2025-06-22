99ad07dcdb78e773b375d1f2f34451a461f7a912
AN-Fahmy <fahmyan@taibaconnect.com>
2025-06-22 21:37:12 +0300

    add scroll spy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from "./components/home/home.component";
@@ -8,11 +8,12 @@ import { PartenersComponent } from "./components/parteners/parteners.component";
 import { WhoWeAreComponent } from "./components/who-we-are/who-we-are.component";
 import { NgwWowService } from 'ngx-wow';
 import { FooterComponent } from "./components/footer/footer.component";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent, OurProjectsComponent, ContractingComponent, PartenersComponent, WhoWeAreComponent, FooterComponent],
+  imports: [NavbarComponent, HomeComponent, OurProjectsComponent, ContractingComponent, PartenersComponent, WhoWeAreComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,5 +21,15 @@ export class AppComponent {
   title = 'MAMLKT-ELATAA';
   constructor(private wowService:NgwWowService){
     this.wowService.init()
+  }
+
+  showArrow:boolean = false
+  @HostListener('window:scroll') onScroll(){
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 300) {
+      this.showArrow = true
+    } else {
+      this.showArrow = false
+    }
   }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  activeSection: string = 'home';
 
+  sections = ['home', 'project', 'contracting', 'parteners', 'whoWeAre'];
+
+  ngOnInit(): void {
+    this.onScroll();
+  }
+
+  @HostListener('window:scroll', []) onScroll(): void {
+    for (const sectionId of this.sections) {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 100 && rect.bottom >= 100) {
+          this.activeSection = sectionId;
+          break;
+        }
+      }
+    }
+  }
+
+  setActive(sectionId: string) {
+    this.activeSection = sectionId;
+  }
 }
