c55c0f15ff7ad34f916019273a4740f1b7cef6cd
AN-Fahmy <fahmyan@taibaconnect.com>
2025-09-17 15:13:01 +0300

    last update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withViewTransitions()),
+  providers: [provideRouter(routes, withViewTransitions(), withHashLocation()),
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideAnimations(),
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,13 +10,13 @@ import { WhoWeArePageComponent } from './components/who-we-are-page/who-we-are-p
 import { ContractingPageComponent } from './components/contracting-page/contracting-page.component';
 
 export const routes: Routes = [
-  {path:'', component:BlankComponent, title:'Mamlkt Al Ataa'},
-  {path:'our-project', component:OurProjectPageComponent, title:'Our Project'},
-  {path:'contracting', component:ContractingPageComponent, title:'Contracting'},
-  {path:'old-project', component:OldProjectPageComponent, title:'Old Project'},
-  {path:'project-offer', component:ProjectofferPageComponent, title:'Projects Offers'},
-  {path:'about-us', component:WhoWeArePageComponent, title:'About Us'},
-  {path:'project-details/:type/:id', component:ProjectDetailsComponent, title:'Project details'},
-  {path:'previour-investment/:id', component:InvestmentDetailsComponent, title:'Investment details'},
-  {path:'book/:id', component:BookComponent, title:'Book Now'}
+  {path:'', component:BlankComponent, title:'Sumou'},
+  {path:'our-project', component:OurProjectPageComponent, title:'Sumou | Our Project'},
+  {path:'contracting', component:ContractingPageComponent, title:'Sumou | Contracting'},
+  {path:'old-project', component:OldProjectPageComponent, title:'Sumou | Old Project'},
+  {path:'project-offer', component:ProjectofferPageComponent, title:'Sumou | Projects Offers'},
+  {path:'about-us', component:WhoWeArePageComponent, title:'Sumou | About Us'},
+  {path:'project-details/:type/:id', component:ProjectDetailsComponent, title:'Sumou | Project details'},
+  {path:'previour-investment/:id', component:InvestmentDetailsComponent, title:'Sumou | Investment details'},
+  {path:'book/:id', component:BookComponent, title:'Sumou | Book Now'}
 ];
